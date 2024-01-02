3e30bc7e6921e7ff9d4b30e6ab0fc7aa3455d93a
MonicaIzzo <info@monicaizzo.net>
2024-01-02 21:20:36 +0100

    format HTML, CSS, JS

--- js/script.js
+++ js/script.js
@@ -19,62 +19,38 @@ Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
 Scott Estrada |	Developer            |	scott-estrada-developer.jpg
 Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg */
 
-console.log("JS OK");
+console.log('JS OK')
 
 // # MILESTONE 0 e 1: CREO l'ARREY "Team" e STAMPO IN CANSOLE.
 
 const teamMembers = [
-  {
-    name: "Wayne Barnett",
-    role: "Founder & CEO",
-    image: "wayne-barnett-founder-ceo.jpg",
-  },
-  {
-    name: "Angela Caroll",
-    role: "Chief Editor ",
-    image: "angela-caroll-chief-editor.jpg",
-  },
-  {
-    name: "Walter Gordon",
-    role: "Office Manager ",
-    image: "walter-gordon-office-manager.jpg",
-  },
-  {
-    name: "Angela Lopez ",
-    role: "Social Media Manager",
-    image: "angela-lopez-social-media-manager.jpg",
-  },
-  {
-    name: "Scott Estrada ",
-    role: "Developer",
-    image: "scott-estrada-developer.jpg",
-  },
-  {
-    name: "Barbara Ramos",
-    role: "Graphic Designer",
-    image: "barbara-ramos-graphic-designer.jpg",
-  },
+    {name: 'Wayne Barnett', role: 'Founder & CEO', image: 'wayne-barnett-founder-ceo.jpg',},
+    {name: 'Angela Caroll', role: 'Chief Editor ', image: 'angela-caroll-chief-editor.jpg',},
+    {name: 'Walter Gordon', role: 'Office Manager ', image: 'walter-gordon-office-manager.jpg',},
+    {name: 'Angela Lopez ', role: 'Social Media Manager', image: 'angela-lopez-social-media-manager.jpg',},
+    {name: 'Scott Estrada ', role: 'Developer', image: 'scott-estrada-developer.jpg',},
+    {name: 'Barbara Ramos', role: 'Graphic Designer', image: 'barbara-ramos-graphic-designer.jpg',},
 ];
 console.log(teamMembers);
 
 for (let i = 0; i < teamMembers.length; i++) {
-  const member = teamMembers[i];
-  console.log(member.name, member.role, member.image);
+    const member = teamMembers[i];
+    console.log(member.name, member.role, member.image);
 }
 
 // MILESTONE 2 e 3 : STAMPO IN DOM l'ARRAY con i DATI testuali e LA FOTO CARICATA DINAMICAMENTE.
 
-const teamContainer = document.getElementById("team-container");
+const teamContainer = document.getElementById('team-container');
 
 // card
 
-let cards = "";
+let cards ='';
 
 for (const member of teamMembers) {
-  const card = `
+    const card = `
     <div class="col-4 mt-3 mb-3">
         <div class="card mb-3 border border-0 rounded-0">
-            <img class="card-img-top rounded-0" src="img/${member.image}" alt=“pippo”>
+            <img class="card-img-top rounded-0" src="img/${member.image}" alt=“${member.name}”>
             <div class="card-body">
                 <h5 class="card-title">${member.name}</h5>
                     <p class=" card-text">${member.role}</p>
@@ -82,6 +58,6 @@ for (const member of teamMembers) {
         </div>
     </div>
     `;
-  cards += card;
+    cards += card;
 }
 teamContainer.innerHTML = cards;
